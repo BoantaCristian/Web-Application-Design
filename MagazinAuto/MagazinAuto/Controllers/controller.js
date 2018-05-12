2c3b239e4468517f91827d66249102a9546d5162
Boanta Ionut Cristian <cristi_cristi222@yahoo.com>
2018-05-12 20:30:02 +0300

    Project

---
+++ MagazinAuto/MagazinAuto/Controllers/controller.js
@@ -0,0 +1,43 @@
+﻿
+    app.controller('MainController', ['$scope', function ($scope) {
+        $scope.products = [
+            {
+                name: 'item1',
+                price: 19,
+                pubdate: new Date('2014', '03', '08'),
+                cover: 'img/turbo.jpg',
+                likes: 0,
+                dislikes: 0
+            },
+            {
+                name: 'item2',
+                price: 8,
+                pubdate: new Date('2013', '08', '01'),
+                cover: 'img/turbo.jpg',
+                likes: 0,
+                dislikes: 0
+            },
+            {
+                name: 'item3',
+                price: 11.99,
+                pubdate: new Date('1999', '07', '08'),
+                cover: 'img/turbo.jpg',
+                likes: 0,
+                dislikes: 0
+            },
+            {
+                name: 'item4',
+                price: 7.99,
+                pubdate: new Date('2011', '08', '16'),
+                cover: 'img/turbo.jpg',
+                likes: 0,
+                dislikes: 0
+            }
+        ];
+        $scope.plusOne = function (index) {
+            $scope.products[index].likes += 1;
+        },
+            $scope.minusOne = function (index) {
+                $scope.products[index].dislikes += 1;
+            }
+    }]}
